88e9e8ea7a14eab549c07ad63a34dfc7f5be9b48
Thijs Deelen <deelenthijs@gmail.com>
2017-06-08 18:22:33 +0200

    'testing'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
-//var status = 1;
+var status = 1;
 
 app.set('port', (process.env.PORT || 5000))
 
