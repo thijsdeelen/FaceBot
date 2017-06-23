821efb8540982e300e9d4cbb5e2b5dcdcd46132a
Thijs Deelen <deelenthijs@gmail.com>
2017-06-23 13:42:23 +0200

    facebotje

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ function getPaymentURL()
   const option = {
     url: 'https://cmprojectgroep1.herokuapp.com/tickets/ticketBestellen',
     method: 'POST',
-    body: {"debitor_reference": "Thijs Deelen", "total_amount": "14,99"}
+    body: {"debitor_reference": "Thijs Deelen", "total_amount": "14,99"},
     json: true,
     headers: {
       "Content-Type":"application/json"
